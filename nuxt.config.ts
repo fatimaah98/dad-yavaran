de2338ea3c46f4495873fc339cbcc7c3de5824d7
fatimaah <fatimah.nassari98@gmail.com>
2025-12-27 01:03:25 +0330

    deploy on github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
-  // nitro: {
-  //   preset: 'static'
-  // },
+  nitro: {
+    preset: 'github-pages'
+  },
   css: ["./app/assets/styles/main.css"],
   vite: {
     plugins: [ tailwindcss()]
@@ -16,7 +16,8 @@ export default defineNuxtConfig({
       htmlAttrs: {
         dir: 'rtl'
       }
-    }
+    },
+    baseURL: '/dad-yavaran/'
   },
   modules: ['@vueuse/motion/nuxt'],
 })
